c2af015c00994cb27572e1339d9cac7d8d9f6015
Leo Schultheiss <leo.schultheiss@tum.de>
2024-08-29 11:53:48 +0200

    update home manager system configurations

--- users/leo/nixos.nix
+++ users/leo/nixos.nix
@@ -5,10 +5,17 @@
   #
   ##################################################################################################################
 
-  users.users.ryan = {
+  users.users.leo = {
     # Leo's authorizedKeys
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8pF+wbcc5Bnd8UvK7KrOEe18vXi2yarfUOhbmrt7Pp leo@nixos"
     ];
+
+    packages = with pkgs; [
+      kdePackages.kate
+      thunderbird
+      keepassxc
+    ];
+    shell = pkgs.fish;
   };
 }
