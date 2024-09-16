13bebfea27668cf147ff98946e45ccb1b259c936
Leo Schultheiss <leo.schultheiss@tum.de>
2024-09-16 08:52:43 +0200

    added shell.nix

---
+++ home/programs/shell.nix
@@ -0,0 +1,19 @@
+{pkgs-unstable, ...}: let
+  shellAliases = {
+    reswitch = "sudo nixos-rebuild switch --flake ~/nixos#laptop";
+  };
+in {
+  # only works in bash/zsh, not nushell
+  home.shellAliases = shellAliases;
+
+  programs.fish = {
+    enable = true;
+#     enableCompletion = true;
+#     bashrcExtra = ''
+#       export PATH="$HOME/.local/bin:$HOME/go/bin:$PATH"
+#     '';
+    interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+      '';
+  };
+}
