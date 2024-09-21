b37d085536c1a900b4f734f4b5ffd4172aebd928
Leo Schultheiss <leo.schultheiss@tum.de>
2024-09-21 21:34:20 +0200

    move firefox profile to browsers.nix

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -1,7 +1,7 @@
 { 
   config, 
   pkgs, 
-#   username,
+   username,
   ... 
 }:
 let
@@ -22,7 +22,31 @@ in {
     firefox = {
       enable = true;
 
-#       profiles.${username} = {};
+       profiles.${username} = {
+                                       id = 0;
+                                       name = "default";
+                                       isDefault = true;
+                                       settings = {
+                                         "browser.startup.homepage" = "https://duckduckgo.com";
+                                         "browser.search.defaultenginename" = "DuckDuckGo";
+                                         "browser.search.order.1" = "DuckDuckGo";
+                                         "signon.rememberSignons" = false;
+
+                                         "widget.use-xdg-desktop-portal.file-picker" = 1;
+                                         "browser.aboutConfig.showWarning" = false;
+                                         "browser.compactmode.show" = true;
+                                         "browser.cache.disk.enable" = false; # Be kind to hard drive
+
+                                         #"mousewheel.default.delta_multiplier_x" = 20;
+                                         #"mousewheel.default.delta_multiplier_y" = 20;
+                                         #"mousewheel.default.delta_multiplier_z" = 20;
+                                       };
+                                       search = {
+                                         force = true;
+                                         default = "DuckDuckGo";
+                                         order = [ "DuckDuckGo" "Google" ];
+                                       };
+                                     };
 
       /* ---- POLICIES ---- */
       # Check about:policies#documentation for options.
--- users/leo/home.nix
+++ users/leo/home.nix
@@ -7,9 +7,6 @@
 
   imports = [
     ../../home/core.nix
-
-#     ../../home/fcitx5
-#     ../../home/i3
     ../../home/programs
 #     ../../home/rofi
 #     ../../home/shell
@@ -21,34 +18,34 @@
       userEmail = "leo.schultheiss@tum.de";
     };
 
-    firefox = {
-      enable = true;
-      profiles.default = {
-        id = 0;
-        name = "default";
-        isDefault = true;
-        settings = {
-          "browser.startup.homepage" = "https://duckduckgo.com";
-          "browser.search.defaultenginename" = "DuckDuckGo";
-          "browser.search.order.1" = "DuckDuckGo";
-          "signon.rememberSignons" = false;
-
-          "widget.use-xdg-desktop-portal.file-picker" = 1;
-          "browser.aboutConfig.showWarning" = false;
-          "browser.compactmode.show" = true;
-          "browser.cache.disk.enable" = false; # Be kind to hard drive
-
-          #"mousewheel.default.delta_multiplier_x" = 20;
-          #"mousewheel.default.delta_multiplier_y" = 20;
-          #"mousewheel.default.delta_multiplier_z" = 20;
-        };
-        search = {
-          force = true;
-          default = "DuckDuckGo";
-          order = [ "DuckDuckGo" "Google" ];
-        };
-      };
-    };
+#    firefox = {
+#      enable = true;
+#      profiles.default = {
+#        id = 0;
+#        name = "default";
+#        isDefault = true;
+#        settings = {
+#          "browser.startup.homepage" = "https://duckduckgo.com";
+#          "browser.search.defaultenginename" = "DuckDuckGo";
+#          "browser.search.order.1" = "DuckDuckGo";
+#          "signon.rememberSignons" = false;
+#
+#          "widget.use-xdg-desktop-portal.file-picker" = 1;
+#          "browser.aboutConfig.showWarning" = false;
+#          "browser.compactmode.show" = true;
+#          "browser.cache.disk.enable" = false; # Be kind to hard drive
+#
+#          #"mousewheel.default.delta_multiplier_x" = 20;
+#          #"mousewheel.default.delta_multiplier_y" = 20;
+#          #"mousewheel.default.delta_multiplier_z" = 20;
+#        };
+#        search = {
+#          force = true;
+#          default = "DuckDuckGo";
+#          order = [ "DuckDuckGo" "Google" ];
+#        };
+#      };
+#    };
 
     thunderbird = {
       enable = true;
