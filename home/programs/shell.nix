4f2779f3e75f67290eb8fa5f5651caa2e760abe0
Leo Schultheiss <leo.schultheiss@tum.de>
2024-09-22 10:22:13 +0200

    add starship shell to shell.nix

--- home/programs/shell.nix
+++ home/programs/shell.nix
@@ -1,20 +1,20 @@
 { pkgs-unstable, ... }:
-let
-  shellAliases = {
-    reswitch = "sudo nixos-rebuild switch --flake ~/nixos#laptop";
-  };
-in
 {
-  # only works in bash/zsh, not nushell
-  home.shellAliases = shellAliases;
-
   programs.fish = {
-        enable = true;
-        interactiveShellInit = ''
-          set fish_greeting # Disable greeting
-          function reswitch
-              sudo nixos-rebuild switch --flake ~/nixos#$argv
-          end
-        '';
-      };
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting # Disable greeting
+      function reswitch
+          sudo nixos-rebuild switch --flake ~/nixos#$argv
+      end
+    '';
+  };
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      add_newline = false;
+    };
+  };
 }
