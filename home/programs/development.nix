edfcbfc356b727a95f4a2084a84cfe642c0b4448
Leo Schultheiss <leo.schultheiss@tum.de>
2024-09-23 17:01:56 +0200

    add conda-shell.nix

--- home/programs/development.nix
+++ home/programs/development.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    # IDEs
     jetbrains.idea-ultimate
     jetbrains.pycharm-professional
+
+    # python
     python3
+    conda # anaconda python package manager
+
     nixfmt-rfc-style # nix formatter
   ];
 }
